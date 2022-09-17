8beaab8ed3f5a0af73aecbc092534a04e7196ee6
Pouya <pouya_behrooj@yahoo.com>
2022-09-17 21:30:30 +0430

    app created and have a proper design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './style.css';
 
 function App() {
+   const [count , setCount] = useState(0);
   return (
-    <div>hello pouya welcome back</div>
+    <section >
+
+      <section className="counter-body">
+        <section>
+        <h1>Counter App</h1>
+        <h2>{count}</h2>
+        <section className="buttons">
+          <button className="increase" onClick={()=> setCount(count+1) }>Increase</button>
+          <button className="decrease" onClick={()=> setCount(count-1) } disabled={count===0}>Decrease</button>
+          <button className="reset" onClick={()=> setCount(0) } disabled={count===0}>Reset</button>
+  
+          </section>    
+        </section>
+        </section>
+
+    </section>
+    
   )
 }
 
